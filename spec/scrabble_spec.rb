350885c9779feb5be753dce02fb8a38c772e93ce
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-23 11:33:28 -0800

    Write a third test to return a score for any word.
    Co-authored-by: Kerry Lang <klang812@gmail.com>

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -7,9 +7,13 @@ describe('Score#return_word_score') do
     score = Score.new("e")
     expect(score.return_word_score).to(eq(1))
   end
-  it("returns a scrabble score for a word") do
+  it("returns a scrabble score for word aeiou") do
     score = Score.new("aeiou")
     expect(score.return_word_score).to(eq(5))
+  end
+  it("returns a scrabble score for a any word") do
+    score = Score.new("hello")
+    expect(score.return_word_score).to(eq(8))
   end
 end
 
